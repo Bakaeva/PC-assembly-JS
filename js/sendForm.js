45eebc20b13656d96975d5776552be3abe0065f4
Bakaeva <l_bakaeva74@mail.ru>
2025-11-13 11:44:51 +0300

    отправка данных на сервер через FormData

--- js/sendForm.js
+++ js/sendForm.js
@@ -1,40 +1,34 @@
 const sendForm = () => {
     const form = document.querySelector('.modal');
+    const text = form.querySelector('input[type="text"');
+    const tel = form.querySelector('input[type="tel"');
+    const email = form.querySelector('input[type="email"');
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-        const text = form.querySelector('input[type="text"');
-        const tel = form.querySelector('input[type="tel"');
-        const email = form.querySelector('input[type="email"');
-
-        const sendObj = {
-            name: text.value,
-            phone: tel.value,
-            email: email.value
-        };
-
-        fetch('https://jsonplaceholder.typicode.com/posts', {
+        let formData = new FormData(form);
+        let resMsg = '';
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts', {
             method: 'POST',
-            body: JSON.stringify(sendObj),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            }
+            body: new FormData(form)
         })
-            .then(response => {
+            .then(async (response) => {
                 if (response.status !== 201) {
                     throw new Error('Failed to send data! Status error = ' + response.status);
                 };
-                return response.json();
+                let result = await response.json();
+                resMsg = 'Data sent successfully.';
+                return result;
             })
-            .then(json => { })
-            .catch(error => {
-                alert(error.message);
+            .catch((error) => {
+                resMsg = error.message;
             })
             .finally(() => {
                 text.value = '';
                 tel.value = '';
                 email.value = '';
+                alert(resMsg);
             });
     });
 };
